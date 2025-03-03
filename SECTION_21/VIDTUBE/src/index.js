d1af73f6d94d365fda81e29fa565acf88ee58e73
Aman Kumar <akt9802@gmail.com>
2025-03-03 23:55:07 +0530

    Add environment configuration and database connection setup

--- SECTION_21/VIDTUBE/src/index.js
+++ SECTION_21/VIDTUBE/src/index.js
@@ -1,4 +1,26 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
-console.log("Hello from backend course with chai.... Hello one more time");
+
+dotenv.config({
+    path:"./.env"
+})
+
+const PORT = process.env.PORT || 8001;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+connectDB()
+.then(
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);     
+    })
+)
+.catch((err) => {
+    console.log("Mongodb connection error",err);
+    
+})
 
 
