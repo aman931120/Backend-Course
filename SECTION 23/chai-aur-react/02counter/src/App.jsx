545acc317674f87e445736d043217cf2c677e4a4
Aman Kumar <akt9802@gmail.com>
2025-03-19 00:35:10 +0530

    Add initial setup for Vite and React with counter functionality

---
+++ SECTION 23/chai-aur-react/02counter/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  // problem is UI updation
+
+  let [counter, setCounter] = useState(0)
+  // here set counter is basically a method/function which works on counter
+  // and use state is basically a hook to solve the problem of UI updation
+
+  const addValue = () => {
+    // console.log("value Added",Math.random());
+    // console.log("clicked",counter);
+    counter = counter + 1;
+    setCounter(counter)
+  };
+
+  const removeValue = () =>{
+    counter = counter - 1;
+    setCounter(counter);
+    
+  }
+
+  // let counter = 5
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value : {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add Value </button>
+      <br />
+      <br />
+      <button 
+      onClick={removeValue}
+      >Remove Value</button>
+      {/* <p>footer : </p> */}
+    </>
+  );
+}
+
+export default App
